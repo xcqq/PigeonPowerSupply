a60b65811bbc99709d59f28ad7ccf4df2c06b143
xczhang <xc.zhang@outlook.com>
2024-09-18 23:51:29 +0800

    code clean up for io service

--- src/config.h
+++ src/config.h
@@ -1,5 +1,4 @@
 #define VERSION "0.0.0.4" //version number
-#define _DEBUG_
 #define DEV_TYPE "M5STACK"
 #define ROTATION 1
 
@@ -8,3 +7,5 @@
 #define BUF_SIZE 80
 #define DISP_BUF_SIZE (LV_HOR_RES_MAX * BUF_SIZE)
 #define BUF_NUM 1
+
+#define I2C_SPEED 400000U
--- src/io_service.cpp
+++ src/io_service.cpp
@@ -7,7 +7,6 @@
 io_service::io_service() {}
 io_service::~io_service() {}
 
-
 void IRAM_ATTR io_service::power_module_sync(void) {
     _power_module_status.set_volt = _pps.getOutputVoltage();
     _power_module_status.set_curr = _pps.getOutputCurrent();
@@ -17,6 +16,13 @@ void IRAM_ATTR io_service::power_module_sync(void) {
     _power_module_status.enable_flag = _pps.getPowerEnable();
     _power_module_status.temperature = _pps.getTemperature();
     _power_module_status.in_volt = _pps.getVIN();
+    if (_power_set_flag) {
+        _pps.setOutputVoltage(_power_module_settings.set_volt);
+        _pps.setOutputCurrent(_power_module_settings.set_curr);
+        _pps.setPowerEnable(_power_module_settings.enable_flag);
+        _power_set_flag = false;
+    }
+#ifdef _DEBUG_
     Serial.printf("Power module sync:\n");
     Serial.printf("set volt:%.3f\n", _power_module_status.set_volt);
     Serial.printf("set curr:%.3f\n", _power_module_status.set_curr);
@@ -26,12 +32,7 @@ void IRAM_ATTR io_service::power_module_sync(void) {
     Serial.printf("enable flag:%d\n", _power_module_status.enable_flag);
     Serial.printf("temperature:%f\n", _power_module_status.temperature);
     Serial.printf("in volt:%f\n", _power_module_status.in_volt);
-    if (_power_set_flag) {
-        _pps.setOutputVoltage(_power_module_settings.set_volt);
-        _pps.setOutputCurrent(_power_module_settings.set_curr);
-        _pps.setPowerEnable(_power_module_settings.enable_flag);
-        _power_set_flag = false;
-    }
+#endif
 }
 
 void IRAM_ATTR io_service::hmi_module_sync(void) {
@@ -40,10 +41,12 @@ void IRAM_ATTR io_service::hmi_module_sync(void) {
     _hmi_module_status.button_2 = _hmi.getButton2();
     _hmi_module_status.button_s = _hmi.getButtonS();
     _hmi_module_status.encoder_inc_raw += encoder_inc;
+#ifdef _DEBUG_
     Serial.printf("Power module sync:\n");
     Serial.printf("btn1:%d btn2:%d btns:%d\n", _hmi_module_status.button_1,
                   _hmi_module_status.button_2, _hmi_module_status.button_s);
     Serial.printf("encoder inc:%d\n", _hmi_module_status.encoder_inc_raw);
+#endif
 }
 
 struct hmi_module_status IRAM_ATTR io_service::get_hmi_module_status(void) {
@@ -59,12 +62,13 @@ void IRAM_ATTR io_service::set_power_module_status(struct power_module_settings 
     _power_set_flag = true;
 }
 
-void IRAM_ATTR io_service::setup()
+void ICACHE_FLASH_ATTR io_service::setup()
 {
     Serial.begin(115200);
+
     Serial.printf("==========Init power module==========\n");
     uint32_t uid[3];
-    while (!_pps.begin(&Wire, 21, 22, MODULE_POWER_ADDR, 100000U)) {
+    while (!_pps.begin(&Wire, SDA, SCL, MODULE_POWER_ADDR, I2C_SPEED)) {
         Serial.println("module pps connect error");
         delay(100);
     }
@@ -74,21 +78,21 @@ void IRAM_ATTR io_service::setup()
     _pps.getUID(&uid[0], &uid[1], &uid[2]);
     Serial.printf("Power module UID:0x%x%x%x\n", uid[0],uid[1],uid[2]);
     Serial.printf("====================\n");
+
     Serial.printf("==========Init hmi module==========\n");
-    while (!_hmi.begin(&Wire)) {
+    while (!_hmi.begin(&Wire, HMI_ADDR, SDA, SCL, I2C_SPEED)) {
         Serial.println("module hmi connect error");
         delay(100);
     }
     _hmi.resetCounter();
     Serial.printf("====================\n");
+
     M5.Speaker.begin();
     M5.Speaker.mute();
-
-} // end io setup
+}
 
 void IRAM_ATTR io_service::loop()
 {
     power_module_sync();
     hmi_module_sync();
-    // delay(100);
-} // end loop
+}
